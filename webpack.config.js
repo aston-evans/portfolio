81f6fae2cfb5251c384d68bf5eee4d923b6fd0e1
me <findaston@gmail.com>
2025-02-26 21:59:08 -0500

    trying webpack, for bootstrap and poppers

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/import.js", // Entry point for your JavaScript
+    output: {
+        filename: "bundle.js", // Output bundled file
+        path: path.resolve(__dirname, "dist"), // Output directory
+        publicPath: "/", // Make sure to serve files from root
+    },
+    watch: true, 
+    module: {
+        rules: [
+            {
+                test: /\.css$/, // CSS rule
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/home.html", // Path to your HTML
+            filename: "home.html", // Output filename in dist/
+            //chunks: ["home", "shared"]
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/about.html", // Path to your HTML
+            filename: "about.html", // Output filename in dist/
+            //chunks: ["about", "shared"]
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/contact.html", // Path to your HTML
+            filename: "contact.html", // Output filename in dist/
+            //chunks: ["contact", "shared"]
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/project.html", // Path to your HTML
+            filename: "project.html", // Output filename in dist/
+            //chunks: ["project", "shared"]
+        }),
+        
+    ],
+    devServer: {
+        static: path.join(__dirname, "dist"), // Serve content from dist/
+        compress: true,
+        port: 8080, // Port for the server
+    },
+    mode: "development", // Use "production" for production mode
+};
