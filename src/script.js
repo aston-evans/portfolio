7c5d2c7a04f1e12f4e8cf8bb8f4a7231d351b4f7
me <findaston@gmail.com>
2025-03-30 18:01:10 -0400

    added animation

--- src/script.js
+++ src/script.js
@@ -1,2 +1,12 @@
 import anime from 'animejs/lib/anime.es.js';
 
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevents page reload
+
+    let alertBox = document.getElementById("alertMessage");
+    alertBox.style.display = "block"; // Show message
+
+    setTimeout(() => {
+        alertBox.style.display = "none"; // Hide message after 3 seconds
+    }, 3000);
+});
